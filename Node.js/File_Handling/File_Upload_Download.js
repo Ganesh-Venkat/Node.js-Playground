a841b335ae0772ee412b15c69891ee60518744e7
Ganesh-Venkat <ganeshvenkat@ideas2it.com>
2018-12-10 19:01:11 +0530

    Added file operations

--- Node.js/File_Handling/File_Upload_Download.js
+++ Node.js/File_Handling/File_Upload_Download.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({
-  dest: 'uploads/' // this saves your file into a directory called "uploads"
+
+const storage = multer.diskStorage({
+  destination: 'uploads/', // this saves your file into a directory called "uploads"
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
 }); 
+var upload = multer({ storage: storage });
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/file', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 // It's very crucial that the file name matches the name attribute in your html
-app.post('/', upload.single('file-to-upload'), (req, res) => {
-  res.redirect('/');
+app.post('/file', upload.single('file-to-upload'), (req, res) => {
+  res.redirect('/file');
 });
 
+// make this generic
+app.get('/download', (req, res) => {
+  res.download('uploads/' + 'dash-bg-01.jpg');
+});
 app.listen(8080);
+
